8c5c4dd6424eee4c34ccd110b80be2e4ece6edaf
Sara Thampi <saraannthampi@gmail.com>
2023-10-26 10:00:00 +0100

    formatted date returned

--- events/events.model.js
+++ events/events.model.js
@@ -1,7 +1,15 @@
 import { pool } from "../db/index.js";
 
+// export async function getEvents() {
+//   const data = await pool.query("SELECT * FROM events ORDER BY date ASC;");
+//   return data.rows;
+// }
+
 export async function getEvents() {
-  const data = await pool.query("SELECT * FROM events ORDER BY date ASC;");
+  const data = await pool.query(`SELECT *,
+  (SELECT to_char(date, 'FMDay, DDth Month YYYY') FROM events e2 WHERE e2.id = events.id) AS formatted_date
+FROM events
+ORDER BY date ASC;`);
   return data.rows;
 }
 
